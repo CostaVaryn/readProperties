bdc980e3bc1a16fc8e0607cbf6dca97c5f0d038a
Constantin <53481895+CostaVaryn@users.noreply.github.com>
2022-09-04 17:07:33 +0300

    Update BaseTest.java

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 
 abstract public class BaseTest {
 
-    public void setUp(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
@@ -17,12 +17,12 @@ abstract public class BaseTest {
     }
 
     @BeforeEach
-    public void init(){
+    public void init() {
         setUp();
     }
 
     @AfterEach
-    public void tearDown(){
+    public void tearDown() {
         Selenide.closeWebDriver();
     }
 }
