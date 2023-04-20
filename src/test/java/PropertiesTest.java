79f6489bdf4ff5b3f254fdfb275469456cf9622b
Constantin Varyn <d3tug@yandex.ru>
2023-04-20 09:18:58 +0300

    feat: add comments

--- src/test/java/PropertiesTest.java
+++ src/test/java/PropertiesTest.java
@@ -4,24 +4,24 @@ import org.junit.Test;
 import java.io.IOException;
 
 public class PropertiesTest extends BaseTest {
-    
+
     @Test
     public void readProperties() throws IOException {
         System.getProperties().load(ClassLoader.getSystemResourceAsStream("application.properties"));
         String urlFromProperty = System.getProperty("url");
-        System.out.println(urlFromProperty);
+        System.out.println("URL from properties: " + urlFromProperty);
     }
-    
+
     @Test
     public void readFromConf() {
         String urlFromConf = ConfigProvider.URL;
-        System.out.println(urlFromConf);
-        Boolean isViniAdmin = ConfigProvider.IS_VINI_ADMIN;
+        System.out.println("URL from config: " + urlFromConf);
+        Boolean isViniAdmin = ConfigProvider.IS_USER_ADMIN;
         System.out.println(isViniAdmin);
-        if(ConfigProvider.readConfig().getBoolean("usersParams.admin.isAdmin")){
+        if (ConfigProvider.readConfig().getBoolean("usersParams.admin.isAdmin")) {
             System.out.println("Admin is really admin");
         } else {
-            System.out.println("Impostor");
+            System.out.println("Get out, Impostor!");
         }
     }
 }
