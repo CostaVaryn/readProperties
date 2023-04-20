7706885ff446a9c52afb8b53ce76be3826699e29
Constantin Varyn <d3tug@yandex.ru>
2023-04-20 09:23:52 +0300

    add comment

--- src/test/java/PropertiesTest.java
+++ src/test/java/PropertiesTest.java
@@ -16,8 +16,8 @@ public class PropertiesTest extends BaseTest {
     public void readFromConf() {
         String urlFromConf = ConfigProvider.URL;
         System.out.println("URL from config: " + urlFromConf);
-        Boolean isViniAdmin = ConfigProvider.IS_USER_ADMIN;
-        System.out.println(isViniAdmin);
+        Boolean isUserAdmin = ConfigProvider.IS_USER_ADMIN;
+        System.out.println("User is admin: " + isUserAdmin);
         if (ConfigProvider.readConfig().getBoolean("usersParams.admin.isAdmin")) {
             System.out.println("Admin is really admin");
         } else {
