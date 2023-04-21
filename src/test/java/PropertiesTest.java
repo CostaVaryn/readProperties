0029cd91679bd8e769cef4004bf375a34606c23d
Constantin Varyn <d3tug@yandex.ru>
2023-04-21 19:03:12 +0300

    refactor test

--- src/test/java/PropertiesTest.java
+++ src/test/java/PropertiesTest.java
@@ -14,10 +14,9 @@ public class PropertiesTest extends BaseTest {
 
     @Test
     public void readFromConf() {
-        String urlFromConf = ConfigProvider.URL;
-        System.out.println("URL from config: " + urlFromConf);
-        Boolean isUserAdmin = ConfigProvider.IS_USER_ADMIN;
-        System.out.println("User is admin: " + isUserAdmin);
+        System.out.println("URL from config: " + ConfigProvider.URL);
+        System.out.println("User is admin: " + ConfigProvider.USER_IS_ADMIN);
+        System.out.println("Admin is admin: " + ConfigProvider.ADMIN_IS_ADMIN);
         if (ConfigProvider.readConfig().getBoolean("usersParams.admin.isAdmin")) {
             System.out.println("Admin is really admin");
         } else {
